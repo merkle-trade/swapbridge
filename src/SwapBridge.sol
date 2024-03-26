a21047ca5991e327bfbd5269777d7d07a9f1c226
Ye <ye@tauruslabs.xyz>
2024-03-27 02:09:43 +0900

    done!

--- src/SwapBridge.sol
+++ src/SwapBridge.sol
@@ -1,35 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "forge-std/console.sol";
+import {ITokenBridge} from "LayerZero-Aptos-Contract/apps/bridge-evm/contracts/interfaces/ITokenBridge.sol";
+import {LzLib} from "@layerzerolabs/solidity-examples/contracts/libraries/LzLib.sol";
 
 contract SwapBridge {
-    function approveMax(IERC20 token, address spender) public {
-        SafeERC20.forceApprove(token, spender, type(uint256).max);
+    uint256 constant APT_AIRDROP_AMOUNT = 9904;
+
+    ITokenBridge immutable tokenBridge;
+
+    constructor(address _tokenBridgeAddress) {
+        tokenBridge = ITokenBridge(_tokenBridgeAddress);
     }
 
-    function swap(
-        IERC20 fromToken,
-        uint256 fromAmount,
-        IERC20 toToken,
-        address swapTarget,
-        bytes calldata swapBytes
-    ) public {
-        SafeERC20.safeTransferFrom(
-            fromToken,
-            msg.sender,
-            address(this),
-            fromAmount
-        );
+    function approveMax(IERC20 _token, address _spender) public {
+        SafeERC20.forceApprove(_token, _spender, type(uint256).max);
+    }
 
-        SafeERC20.forceApprove(fromToken, swapTarget, type(uint256).max);
-        (bool success, ) = swapTarget.call(swapBytes);
-        require(success, "Swap failed");
-
-        SafeERC20.safeTransfer(fromToken, msg.sender, fromToken.balanceOf(address(this)));
-        SafeERC20.safeTransfer(toToken, msg.sender, toToken.balanceOf(address(this)));
+    function quoteForSend() public view returns (uint256 nativeFee, uint256 zroFee) {
+        (LzLib.CallParams memory callParams, bytes memory adapterParams) = _lzParams(hex"01");
+        (nativeFee, zroFee) = tokenBridge.quoteForSend(callParams, adapterParams);
     }
 
     function swapAndSendToAptos(
@@ -37,11 +30,56 @@ contract SwapBridge {
         uint256 fromAmount,
         IERC20 toToken,
         uint256 toAmount,
+        bytes32 aptosAddress,
         address swapTarget,
         bytes calldata swapBytes
-    ) public {
-        swap(fromToken, fromAmount, toToken, swapTarget, swapBytes);
+    ) public payable {
+        uint256 actualToAmount;
+        {
+            SafeERC20.safeTransferFrom(fromToken, msg.sender, address(this), fromAmount);
 
+            (bool success,) = swapTarget.call(swapBytes);
+            require(success, "Swap failed");
+
+            actualToAmount = toToken.balanceOf(address(this));
+            require(actualToAmount >= toAmount, "toAmount");
+
+            if (fromToken.balanceOf(address(this)) > 0) {
+                SafeERC20.safeTransfer(fromToken, msg.sender, fromToken.balanceOf(address(this)));
+            }
+        }
+
+        {
+            SafeERC20.forceApprove(toToken, address(tokenBridge), type(uint256).max);
+            (LzLib.CallParams memory callParams, bytes memory adapterParams) = _lzParams(aptosAddress);
+            tokenBridge.sendToAptos{value: msg.value}(
+                address(toToken), aptosAddress, toToken.balanceOf(address(this)), callParams, adapterParams
+            );
+        }
+
+        require(toToken.balanceOf(address(this)) == 0, "toToken remaining");
     }
 
+    function _lzParams(bytes32 aptosAddress)
+        private
+        view
+        returns (LzLib.CallParams memory callParams, bytes memory adapterParams)
+    {
+        callParams = LzLib.CallParams({refundAddress: payable(msg.sender), zroPaymentAddress: address(0x0)});
+        adapterParams = LzLib.buildAirdropAdapterParams(
+            10000, // uaGas
+            LzLib.AirdropParams({airdropAmount: APT_AIRDROP_AMOUNT, airdropAddress: aptosAddress})
+        );
+    }
 }
+
+// TODO layerzero msg 의 msg.sender 대신 tx 의 from 을 봐야 함 (tx.origin)
+// TODO paraswap build tx 할 때 userAddress, txOrigin, receiver
+
+// txType 2
+// bytes  [2       32        32            bytes[]         ]
+// fields [txType  extraGas  dstNativeAmt  dstNativeAddress]
+// 0002
+// 0000000000000000000000000000000000000000000000000000000000002710
+// 0000000000000000000000000000000000000000000000000000000000000000
+// 1cc57f0fe249bc7278c2e38e85a85ca6c9e0ee51b47ff073797c31702d93a4cb
--- test/SwapBridge.t.sol
+++ test/SwapBridge.t.sol
@@ -10,13 +10,12 @@ contract SwapBridgeTest is Test {
     IERC20 constant usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     IERC20 constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
-    address deployer;
     address constant fromUser = 0x8558FE88F8439dDcd7453ccAd6671Dfd90657a32; // some rich guy with 8-digit USDT blance
+    address constant lzTokenBridge = 0x50002CdFe7CCb0C41F519c6Eb0653158d11cd907;
+    address constant paraswapAugustusSwapper = 0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57;
+    address constant paraswapTokenTransferProxy = 0x216B4B4Ba9F3e719726886d34a177484278Bfcae;
 
-    address constant paraswapAugustusSwapper =
-        0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57;
-    address constant paraswapTokenTransferProxy =
-        0x216B4B4Ba9F3e719726886d34a177484278Bfcae;
+    address deployer;
 
     SwapBridge public swapBridge;
 
@@ -28,37 +27,50 @@ contract SwapBridgeTest is Test {
         assertEq(deployer, 0xaE0bDc4eEAC5E950B67C6819B118761CaAF61946);
 
         hoax(deployer);
-        swapBridge = new SwapBridge();
-        assertEq(
-            address(swapBridge),
-            0x8Ad159a275AEE56fb2334DBb69036E9c7baCEe9b
-        );
+        swapBridge = new SwapBridge(lzTokenBridge);
+        assertEq(address(swapBridge), 0x8Ad159a275AEE56fb2334DBb69036E9c7baCEe9b);
+
+        // fromUser
+        assertGt(usdt.balanceOf(fromUser), 100_000_000_000);
+        assertEq(usdc.balanceOf(fromUser), 0);
     }
 
-    function test_ParaSwap() public {
+    function test_SwapAndSendToAptos_ParaSwap() public {
         swapBridge.approveMax(usdt, paraswapTokenTransferProxy);
+        (uint256 nativeFee,) = swapBridge.quoteForSend();
 
         startHoax(fromUser);
-        SafeERC20.safeIncreaseAllowance(
-            usdt,
-            address(swapBridge),
-            1_000_000_000
-        );
-        swapBridge.swap(
+        SafeERC20.safeIncreaseAllowance(usdt, address(swapBridge), 1_000_000_000);
+        swapBridge.swapAndSendToAptos{value: nativeFee}(
             usdt,
             1_000_000_000, // 1000 USDT
             usdc,
+            990_000_000,
+            hex"01",
             paraswapAugustusSwapper,
             hex"0b86a4c1000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000000000000000000000000000000000003b9aca00000000000000000000000000000000000000000000000000000000003b689ce1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000001000000000000000000004de53041cbd36888becc7bbcbc0045e3b1f144466f5f"
         );
-        assertGt(usdt.balanceOf(fromUser), 0);
-        assertGt(usdc.balanceOf(fromUser), 997_000_000);
-        emit log_uint(usdc.balanceOf(fromUser));
+        assertEq(address(swapBridge).balance, 0);
+        assertEq(usdc.balanceOf(fromUser), 0);
         assertEq(usdt.balanceOf(address(swapBridge)), 0);
         assertEq(usdc.balanceOf(address(swapBridge)), 0);
     }
 
-    function test_Increment() public {
-        emit log_address(address(this));
+    function test_Swap_ParaSwap_toAmount() public {
+        swapBridge.approveMax(usdt, paraswapTokenTransferProxy);
+        (uint256 nativeFee,) = swapBridge.quoteForSend();
+
+        startHoax(fromUser);
+        SafeERC20.safeIncreaseAllowance(usdt, address(swapBridge), 1_000_000_000);
+        vm.expectRevert("toAmount");
+        swapBridge.swapAndSendToAptos{value: nativeFee}(
+            usdt,
+            1_000_000_000, // 1000 USDT
+            usdc,
+            10_000_000_000,
+            hex"01",
+            paraswapAugustusSwapper,
+            hex"0b86a4c1000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000000000000000000000000000000000003b9aca00000000000000000000000000000000000000000000000000000000003b689ce1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000001000000000000000000004de53041cbd36888becc7bbcbc0045e3b1f144466f5f"
+        );
     }
 }
